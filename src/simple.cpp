8c208f9b52873b5abf07c3cabe4dee7fbc2677d6
celiahermoso <celia.hermosodiaz@epfl.ch>
2020-12-19 11:44:45 +0100

    Extended tt

--- src/simple.cpp
+++ src/simple.cpp
@@ -7,39 +7,47 @@
 
 using namespace std;
 
-bool check( Truth_Table const& tt, string const& ans )
+void check( Truth_Table const& tt, string const& ans )
 {
   cout << "  checking function correctness";
   if ( tt == Truth_Table( ans ) )
   {
     cout << "...passed." << endl;
-    return true;
   }
   else
   {
     cout << "...failed. (expect " << ans << ", but get " << tt << ")" << endl;
-    return false;
   }
 }
 
-bool check( uint64_t dd_size, uint64_t expected )
+void check( Truth_Table const& tt, Truth_Table const& tt2 )
 {
-  cout << "  checking BDD size";
-  if ( dd_size <= expected ) /* Using complemented edges can reduce, but will never increase BDD size. */
+  cout << "  checking function correctness";
+  if ( tt == tt2 )
   {
     cout << "...passed." << endl;
-    return true;
+  }
+  else
+  {
+    cout << "...failed. (expect " << tt2 << ", but get " << tt << ")" << endl;
+  }
+}
+
+void check( uint64_t dd_size, uint64_t expected )
+{
+  cout << "  checking BDD size";
+  if ( dd_size == expected )
+  {
+    cout << "...passed." << endl;
   }
   else
   {
     cout << "...failed. (expect " << expected << ", but get " << dd_size << " nodes)" << endl;
-    return false;
   }
 }
 
 int main()
 {
-  bool passed = true;
   {
     cout << "test 00: x0 XOR x1" << endl;
     BDD bdd( 2 );
@@ -47,10 +55,10 @@ int main()
     auto const x1 = bdd.literal( 1 );
     auto const f = bdd.XOR( x0, x1 );
     auto const tt = bdd.get_tt( f );
-    //bdd.print( f );
-    //cout << tt << endl;
-    passed &= check( tt, "0110" );
-    passed &= check( bdd.num_nodes( f ), 3 );
+    bdd.print( f );
+    cout << tt << endl;
+    check( tt, "0110" );
+    check( bdd.num_nodes( f ), 3 );
   }
 
   {
@@ -60,10 +68,10 @@ int main()
     auto const x1 = bdd.literal( 1 );
     auto const f = bdd.AND( x0, x1 );
     auto const tt = bdd.get_tt( f );
-    //bdd.print( f );
-    //cout << tt << endl;
-    passed &= check( tt, "1000" );
-    passed &= check( bdd.num_nodes( f ), 2 );
+    bdd.print( f );
+    cout << tt << endl;
+    check( tt, "1000" );
+    check( bdd.num_nodes( f ), 2 );
   }
 
   {
@@ -74,11 +82,202 @@ int main()
     auto const x2 = bdd.literal( 2 );
     auto const f = bdd.ITE( x0, x1, x2 );
     auto const tt = bdd.get_tt( f );
+    bdd.print( f );
+    cout << tt << endl;
+    check( tt, "11011000" );
+    check( bdd.num_nodes( f ), 3 );
+  }
+  
+  {
+    cout << "test 03: XOR(x0, XOR(1, 0))" << endl;
+    BDD bdd( 1 );
+    auto const x0 = bdd.literal( 0 );
+    auto const f = bdd.XOR( x0, bdd.XOR( 1, 0 ) );
+    auto const tt = bdd.get_tt( f );
     //bdd.print( f );
     //cout << tt << endl;
-    passed &= check( tt, "11011000" );
-    passed &= check( bdd.num_nodes( f ), 3 );
+    check( tt, "01" );
+    check( bdd.num_nodes( f ), 1 );
   }
 
-  return passed ? 0 : 1;
+  {
+    cout << "test 04: AND(x0, AND(1, 1))" << endl;
+    BDD bdd( 1 );
+    auto const x0 = bdd.literal( 0 );
+    auto const f = bdd.AND( x0, bdd.AND( 1, 1 ) );
+    auto const tt = bdd.get_tt( f );
+    //bdd.print( f );
+    //cout << tt << endl;
+    check( tt, "10" );
+    check( bdd.num_nodes( f ), 1 );
+  }
+  
+  {
+    cout << "test 05: AND(x0, AND(0,0))" << endl;
+    BDD bdd( 1 );
+    auto const x0 = bdd.literal( 0 );
+    auto const f = bdd.AND( x0, bdd.AND( 0, 0 ) );
+    auto const tt = bdd.get_tt( f );
+    //bdd.print( f );
+    //cout << tt << endl;
+    check( tt, "00" );
+    check( bdd.num_nodes( f ), 0 );
+  }
+  
+  {
+    cout << "test 06: XOR(ITE(x0, x1, x2), AND(x0, x3))" << endl;
+    BDD bdd( 4 );
+    auto const x0 = bdd.literal( 0 );
+    auto const x1 = bdd.literal( 1 );
+    auto const x2 = bdd.literal( 2 );
+    auto const x3 = bdd.literal( 3 );
+    auto const f = bdd.XOR( bdd.ITE( x0, x1, x2 ), bdd.AND( x0, x3 ) );
+    auto const tt = bdd.get_tt( f );
+    //bdd.print( f );
+    //cout << tt << endl;
+    check( tt, "0111001011011000" );
+    check( bdd.num_nodes( f ), 5 );
+  }
+  
+  {
+    cout << "test 07: AND(XOR(AND(x0, AND(x3, x4)), x1), x2)\n\
+            == ITE(ITE(ITE(x0, ITE(x3, x4, 0), 0), ITE(x1, 0, 1), x1),\n\t\
+                   ITE(x2, 1, 0), ITE(0, ITE(1, 1, 0), 0))" << endl;
+    BDD bdd( 5 );
+    auto const x0 = bdd.literal( 0 );
+    auto const x1 = bdd.literal( 1 );
+    auto const x2 = bdd.literal( 2 );
+    auto const x3 = bdd.literal( 3 );
+    auto const x4 = bdd.literal( 4 );
+    
+    auto const f = bdd.AND( bdd.XOR( bdd.AND( x0, bdd.AND( x3, x4 ) ), x1 ), x2 );
+    auto const g = bdd.ITE( bdd.ITE( bdd.ITE( x0, bdd.ITE( x3, x4, 0 ), 0 ), bdd.ITE( x1, 0, 1 ), x1 ), 
+                            bdd.ITE( x2, 1, 0 ), bdd.ITE( 0, bdd.ITE( 1, 1, 0 ), 0 ) );
+    auto const tt = bdd.get_tt( f );
+    auto const tt2 = bdd.get_tt( g );
+    //bdd.print( f );
+    //cout << tt << endl;
+    check( tt, tt2 );
+    check( bdd.num_nodes( f ), bdd.num_nodes( g ) );
+  }
+  
+  {
+    cout << "test 08: ITE(x0, x1, x1)" << endl;
+    BDD bdd( 2 );
+    auto const x0 = bdd.literal( 0 );
+    auto const x1 = bdd.literal( 1 );
+    auto const f = bdd.ITE( x0, x1, x1 );
+    auto const tt = bdd.get_tt( f );
+    //bdd.print( f );
+    //cout << tt << endl;
+    check( tt, "1100" );
+    check( bdd.num_nodes( f ), 1 );
+  }
+  
+  {
+    cout << "test 09: ITE(x0, x0, x1)" << endl;
+    BDD bdd( 2 );
+    auto const x0 = bdd.literal( 0 );
+    auto const x1 = bdd.literal( 1 );
+    auto const f = bdd.ITE( x0, x0, x1 );
+    auto const tt = bdd.get_tt( f );
+    //bdd.print( f );
+    //cout << tt << endl;
+    check( tt, "1110" );
+    check( bdd.num_nodes( f ), 2 );
+  }
+  
+  {
+    cout << "test 10: ITE(x0, x1, x0)" << endl;
+    BDD bdd( 2 );
+    auto const x0 = bdd.literal( 0 );
+    auto const x1 = bdd.literal( 1 );
+    auto const f = bdd.ITE( x0, x1, x0 );
+    auto const tt = bdd.get_tt( f );
+    //bdd.print( f );
+    //cout << tt << endl;
+    check( tt, "1000" );
+    check( bdd.num_nodes( f ), 2 );
+  }
+  /* Feel free to add more tests. */
+
+    {
+    cout << "test 11: AND(AND(x0', x0), x1)" << endl;
+    BDD bdd(2);
+    auto const x0 = bdd.literal(0);
+    auto const x0_n = bdd.literal(0, true);
+    auto const x1 = bdd.literal(1);
+    auto const f = bdd.AND(bdd.AND(x0, x0_n), x1);
+    auto const tt = bdd.get_tt(f);
+    //bdd.print( f );
+    //cout << tt << endl;
+    check(tt, "0000");
+    check(bdd.num_nodes(f), 0);
+  }
+
+  {
+    cout << "test 12: AND(AND(x0', x1), x0)" << endl;
+    BDD bdd(2);
+    auto const x0 = bdd.literal(0);
+    auto const x0_n = bdd.literal(0, true);
+    auto const x1 = bdd.literal(1);
+    auto const f = bdd.AND(bdd.AND(x0, x1), x0_n);
+    auto const tt = bdd.get_tt(f);
+    //bdd.print( f );
+    //cout << tt << endl;
+    check(tt, "0000");
+    check(bdd.num_nodes(f), 0);
+  }
+
+
+  {
+    cout << "test 13: AND(XOR(AND(x0', x1), x0), XOR(x2', x3))" << endl;
+    BDD bdd(4);
+    auto const x0 = bdd.literal(0);
+    auto const tt_x0 = create_tt_nth_var(4, 0);
+    auto const x0_n = bdd.literal(0, true);
+    auto const tt_x0_n = create_tt_nth_var(4, 0, false);
+    auto const x1 = bdd.literal(1);
+    auto const tt_x1 = create_tt_nth_var(4, 1);
+    auto const x2_n = bdd.literal(2, true);
+    auto const tt_x2_n = create_tt_nth_var(4, 2, false);
+    auto const x3 = bdd.literal(3);
+    auto const tt_x3 = create_tt_nth_var(4, 3);
+    auto const f = bdd.AND(bdd.XOR(bdd.AND(x0_n, x1), x0), bdd.XOR(x2_n, x3));
+    auto const tt = bdd.get_tt(f);
+
+    auto const expected_tt = ((tt_x0_n & tt_x1) ^ tt_x0) & (tt_x2_n ^ tt_x3);
+    //bdd.print( f );
+    //cout << tt << endl;
+    check(tt, expected_tt);
+    check(bdd.num_nodes(f), 5);
+  }
+
+  {
+    cout << "test 14: ITE(AND(x0', x1), x0, XOR(x2', x3)) === AND(NOT(AND(x0', x1)), XOR(x2', x3))" << endl;
+    BDD bdd(4);
+    auto const x0 = bdd.literal(0);
+    auto const tt_x0 = create_tt_nth_var(4, 0);
+    auto const x0_n = bdd.literal(0, true);
+    auto const tt_x0_n = create_tt_nth_var(4, 0, false);
+    auto const x1 = bdd.literal(1);
+    auto const tt_x1 = create_tt_nth_var(4, 1);
+    auto const x2_n = bdd.literal(2, true);
+    auto const tt_x2_n = create_tt_nth_var(4, 2, false);
+    auto const x3 = bdd.literal(3);
+    auto const tt_x3 = create_tt_nth_var(4, 3);
+    auto const f = bdd.ITE(bdd.AND(x0_n, x1), x0, bdd.XOR(x2_n, x3));
+    auto const tt_t = bdd.get_tt(f);
+
+    auto const g = bdd.AND(bdd.NOT(bdd.AND(x0_n, x1)), bdd.XOR(x2_n, x3));
+    auto const tt_g = bdd.get_tt(g);
+
+    auto const expected_tt = ((tt_x0_n & tt_x1) & tt_x0) | ((~(tt_x0_n & tt_x1)) & (tt_x2_n ^ tt_x3));
+
+    check(tt_t, expected_tt);
+    check(bdd.num_nodes(f), bdd.num_nodes(g));
+    assert(f == g); // Should fail if optimzation is not correct, f and g should be the same
+  }
+
+  return 0;
 }
--- src/truth_table.hpp
+++ src/truth_table.hpp
@@ -4,6 +4,8 @@
 #include <cassert>
 #include <string>
 
+#include <vector>
+
 /* masks used to filter out unused bits */
 static const uint64_t length_mask[] = {
   0x0000000000000001,
@@ -32,17 +34,17 @@ static const uint64_t var_mask_neg[] = {
   0x0000ffff0000ffff,
   0x00000000ffffffff};
 
-/* return i if n == 2^i and i <= 6, 0 otherwise */
+/* return i if n == 2^i and i < 32, 0 otherwise */
 inline uint8_t power_two( const uint32_t n )
 {
   switch( n )
   {
-    case 2u: return 1u;
-    case 4u: return 2u;
-    case 8u: return 3u;
-    case 16u: return 4u;
-    case 32u: return 5u;
-    case 64u: return 6u;
+    case 2u: return 1u; case 4u: return 2u; case 8u: return 3u; case 16u: return 4u; case 32u: return 5u; case 64u: return 6u;
+    case 128u: return 7u; case 256u: return 8u; case 512u: return 9u; case 1024u: return 10u; case 2048u: return 11u; case 4096u: return 12u;
+    case 8192u: return 13u; case 16384u: return 14u; case 32768u: return 15u; case 65536u: return 16u; case 131072u: return 17u; case 262144u: return 18u;
+    case 524288u: return 19u; case 1048576u: return 20u; case 2097152u: return 21u; case 4194304u: return 22u; case 8388608u: return 23u; case 16777216u: return 24u;
+    case 33554432u: return 25u; case 67108864u: return 26u; case 134217728u: return 27u; case 268435456u: return 28u; case 536870912u: return 29u; 
+    case 1073741824u: return 30u; case 2147483648u: return 31u; 
     default: return 0u;
   }
 }
@@ -53,13 +55,17 @@ public:
   Truth_Table( uint8_t num_var )
    : num_var( num_var ), bits( 0u )
   {
-    assert( num_var <= 6u );
+    //assert( num_var <= 32u ); //comment
   }
 
-  Truth_Table( uint8_t num_var, uint64_t bits )
-   : num_var( num_var ), bits( bits & length_mask[num_var] )
+  Truth_Table( uint8_t num_var, uint64_t bits_v )
+   : num_var( num_var ), bits( 1<<num_var )
   {
-    assert( num_var <= 6u );
+    //assert( num_var <= 32u );//comment
+    for (auto i = 1; i<=bits; ++i) {
+        bool tt_vector = bits_v>>(bits-i) & 1; 
+        this->bits_v.push_back(tt_vector);
+    }
   }
 
   Truth_Table( const std::string str )
@@ -96,20 +102,26 @@ public:
     bits &= length_mask[num_var];
   }
 
-  uint8_t n_var() const
+  Truth_Table(uint8_t num_var, std::vector<bool> bits_v)
+  : num_var(num_var), bits_v(bits_v), bits(bits_v.size())
+  {
+  }
+
+  uint32_t n_var() const
   {
     return num_var;
   }
 
-  Truth_Table positive_cofactor( uint8_t const var ) const;
-  Truth_Table negative_cofactor( uint8_t const var ) const;
-  Truth_Table derivative( uint8_t const var ) const;
-  Truth_Table consensus( uint8_t const var ) const;
-  Truth_Table smoothing( uint8_t const var ) const;
+  // Truth_Table positive_cofactor( uint8_t const var ) const;
+  // Truth_Table negative_cofactor( uint8_t const var ) const;
+  // Truth_Table derivative( uint8_t const var ) const;
+  // Truth_Table consensus( uint8_t const var ) const;
+  // Truth_Table smoothing( uint8_t const var ) const;
 
 public:
   uint8_t const num_var; /* number of variables involved in the function */
   uint64_t bits; /* the truth table */
+  std::vector<bool> bits_v; /*number of variables does not have limits*/
 };
 
 /* overload std::ostream operator for convenient printing */
@@ -125,28 +137,44 @@ inline std::ostream& operator<<( std::ostream& os, Truth_Table const& tt )
 /* bit-wise NOT operation */
 inline Truth_Table operator~( Truth_Table const& tt )
 {
-  return Truth_Table( tt.num_var, ~tt.bits );
+  std::vector<bool> NOT;
+  for (auto i = 0; i < tt.bits_v.size(); i++ ){
+    NOT.emplace_back( ~tt.bits_v[i] );
+  }
+  return Truth_Table( tt.num_var, NOT );
 }
 
 /* bit-wise OR operation */
 inline Truth_Table operator|( Truth_Table const& tt1, Truth_Table const& tt2 )
 {
   assert( tt1.num_var == tt2.num_var );
-  return Truth_Table( tt1.num_var, tt1.bits | tt2.bits );
+  std::vector<bool> OR;
+  for (auto i = 0; i < tt1.bits; i++ ){
+    OR.emplace_back( tt1.bits_v[i] | tt1.bits_v[i]);
+  }
+  return Truth_Table( tt1.num_var, OR );
 }
 
 /* bit-wise AND operation */
 inline Truth_Table operator&( Truth_Table const& tt1, Truth_Table const& tt2 )
 {
   assert( tt1.num_var == tt2.num_var );
-  return Truth_Table( tt1.num_var, tt1.bits & tt2.bits );
+    std::vector<bool> AND;
+  for (auto i = 0; i < tt1.bits; i++ ){
+    AND.emplace_back( tt1.bits_v[i] & tt1.bits_v[i]);
+  }
+  return Truth_Table( tt1.num_var, AND );
 }
 
 /* bit-wise XOR operation */
 inline Truth_Table operator^( Truth_Table const& tt1, Truth_Table const& tt2 )
 {
   assert( tt1.num_var == tt2.num_var );
-  return Truth_Table( tt1.num_var, tt1.bits ^ tt2.bits );
+  std::vector<bool> XOR;
+  for (auto i = 0; i < tt1.bits; i++ ){
+    XOR.emplace_back( tt1.bits_v[i] ^ tt1.bits_v[i]);
+  }
+  return Truth_Table( tt1.num_var, XOR );
 }
 
 /* check if two truth_tables are the same */
@@ -156,7 +184,7 @@ inline bool operator==( Truth_Table const& tt1, Truth_Table const& tt2 )
   {
     return false;
   }
-  return tt1.bits == tt2.bits;
+  return tt1.num_var == tt2.num_var;
 }
 
 inline bool operator!=( Truth_Table const& tt1, Truth_Table const& tt2 )
@@ -164,39 +192,40 @@ inline bool operator!=( Truth_Table const& tt1, Truth_Table const& tt2 )
   return !( tt1 == tt2 );
 }
 
-inline Truth_Table Truth_Table::positive_cofactor( uint8_t const var ) const
-{
-  assert( var < num_var );
-  return Truth_Table( num_var, ( bits & var_mask_pos[var] ) | ( ( bits & var_mask_pos[var] ) >> ( 1 << var ) ) );
-}
+// inline Truth_Table Truth_Table::positive_cofactor( uint8_t const var ) const
+// {
+//   assert( var < num_var );
+//   return Truth_Table( num_var, ( bits & var_mask_pos[var] ) | ( ( bits & var_mask_pos[var] ) >> ( 1 << var ) ) );
+// }
 
-inline Truth_Table Truth_Table::negative_cofactor( uint8_t const var ) const
-{
-  assert( var < num_var );
-  return Truth_Table( num_var, ( bits & var_mask_neg[var] ) | ( ( bits & var_mask_neg[var] ) << ( 1 << var ) ) );
-}
+// inline Truth_Table Truth_Table::negative_cofactor( uint8_t const var ) const
+// {
+//   assert( var < num_var );
+//   return Truth_Table( num_var, ( bits & var_mask_neg[var] ) | ( ( bits & var_mask_neg[var] ) << ( 1 << var ) ) );
+// }
 
-inline Truth_Table Truth_Table::derivative( uint8_t const var ) const
-{
-  assert( var < num_var );
-  return positive_cofactor( var ) ^ negative_cofactor( var );
-}
+// inline Truth_Table Truth_Table::derivative( uint8_t const var ) const
+// {
+//   assert( var < num_var );
+//   return positive_cofactor( var ) ^ negative_cofactor( var );
+// }
 
-inline Truth_Table Truth_Table::consensus( uint8_t const var ) const
-{
-  assert( var < num_var );
-  return positive_cofactor( var ) & negative_cofactor( var );
-}
+// inline Truth_Table Truth_Table::consensus( uint8_t const var ) const
+// {
+//   assert( var < num_var );
+//   return positive_cofactor( var ) & negative_cofactor( var );
+// }
 
-inline Truth_Table Truth_Table::smoothing( uint8_t const var ) const
-{
-  assert( var < num_var );
-  return positive_cofactor( var ) | negative_cofactor( var );
-}
+// inline Truth_Table Truth_Table::smoothing( uint8_t const var ) const
+// {
+//   assert( var < num_var );
+//   return positive_cofactor( var ) | negative_cofactor( var );
+// }
 
 /* Returns the truth table of f(x_0, ..., x_num_var) = x_var (or its complement). */
 inline Truth_Table create_tt_nth_var( uint8_t const num_var, uint8_t const var, bool const polarity = true )
 {
-  assert ( num_var <= 6u && var < num_var );
+  assert ( num_var <= 32u && var < num_var );
+  //assert(var<num_var);
   return Truth_Table( num_var, polarity ? var_mask_pos[var] : var_mask_neg[var] );
 }
